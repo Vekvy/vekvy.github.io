76f50078e394db8807eb117cf7f836158b501de2
Vekvy <152788945+Vekvy@users.noreply.github.com>
2026-05-07 20:18:53 +0800

    refactor: rename backend to development and update assets

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -12,7 +12,7 @@ export async function initProjects(indexUrl) {
     if (projectUrls.length === 0) {
       container.innerHTML = `
                 <div style="opacity: 0.7;">
-                    <p class="md-typescale-body-large">There is nothing here yet</p>
+                    <p class="md-typescale-body-large">There's nothing here yet</p>
                 </div>
             `;
       return;
